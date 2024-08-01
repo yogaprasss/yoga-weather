cf365c2abfaa5d35a50f8f58be2c4b5dfde847dc
yogaprasss <yoga.prase.tyo@icloud.com>
2024-08-01 13:44:06 +0700

    initial commit

---
+++ app.vue
@@ -0,0 +1,16 @@
+<template>
+  <NuxtPage />
+</template>
+
+<style lang="css">
+body {
+  margin: 0;
+  padding: 0 1rem;
+  display: flex;
+  justify-content: center;
+}
+#__nuxt {
+  width: 100%;
+  max-width: 1024px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    Hello World!
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home-page',
+}
+</script>
+
+<style lang="css" scoped></style>
