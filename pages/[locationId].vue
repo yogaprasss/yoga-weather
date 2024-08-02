78cb9f9e7e4911caf182b20f1a83dd3878a6f3e2
yogaprasss <yoga.prase.tyo@icloud.com>
2024-08-02 15:06:37 +0700

    adding fetcher on before create

--- pages/[locationId].vue
+++ pages/[locationId].vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="!isLoading">
+  <div>
     <Head>
       <Title>Weather Forecast for {{ location }} | YogaWeather</Title>
     </Head>
@@ -23,16 +23,17 @@ export default {
     return {
       locationId: '',
       location: '',
-      isLoading: true,
       item: 'item'
     };
   },
-  beforeCreated() {
+  async beforeCreate() {
     this.locationId = this.$route.params.locationId;
-    setTimeout(() => {
-      this.location = locationName[this.locationId];
-      this.isLoading = false;
-    }, 5000);
+    const url = import.meta.env.VITE_LOCATION_URL;
+    const apiKey = import.meta.env.VITE_API_KEY;
+    const query = `?apikey=${apiKey}&q=${this.locationId}`;
+    const result = await fetch(url + query);
+    this.item = await result.json();
+    this.location = this.item[1].LocalizedName;
   }
 }
 </script>
