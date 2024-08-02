b4df8f60ec28011fd5472b0a5342ada293c2d21d
yogaprasss <yoga.prase.tyo@icloud.com>
2024-08-02 13:09:03 +0700

    adding weather page

---
+++ pages/[locationId].vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <Head>
+      <Title>Weather Forecast for {{ location }} | YogaWeather</Title>
+    </Head>
+    {{ locationId }}
+  </div>
+</template>
+
+<script>
+const locationName = {
+  jakarta: 'DKI Jakarta',
+  bandung: 'Bandung',
+  surabaya: 'Surabaya'
+};
+export default {
+  name: 'weather-page',
+  data() {
+    return {
+      locationId: '',
+      location: ''
+    };
+  },
+  created() {
+    this.locationId = this.$route.params.locationId;
+    setTimeout(() => {
+      this.location = locationName[this.locationId];
+    }, 5000);
+  }
+}
+</script>
